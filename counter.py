8a6e7c18174ab56062e54a2d36401b763deecf41
Ketchup9743 <liamlessard20@gmail.com>
2026-09-16 12:40:51 +0000

    moved python code to html file, as it wouldnt work

--- counter.py
+++
@@ -1,6 +0,0 @@
-from pyscript import window
-state = {"counter": 1}
-def trigger_doom_alert():
-    window.alert(f"DOOM COUNTER: {state['counter']}")
-    state["counter"] += 1
-window.setInterval(trigger_doom_alert, 500)
