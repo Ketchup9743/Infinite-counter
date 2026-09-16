3af4c82b553850a5baff26aaf0dd6a79c84e45c9
Ketchup9743 <liamlessard20@gmail.com>
2026-09-16 13:45:54 +0000

    fixed spacing

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-
 let counter = 1;
 let clickCount = 0;
-
 const container = document.getElementById("popup-container");
 const clickCounterDisplay = document.getElementById("click-counter");
-
 function show_click_message() {
     clickCount++;
     if (clickCounterDisplay) {
         clickCounterDisplay.textContent = clickCount;
     }
     window.alert(clickCount % 25 === 0 ? "Why do you keep clicking this?" : "Nothing Here :)");
-}
-
 function spawn_doom_popup() {
     if (container) {
         let popup = container.querySelector(".doom-popup");
