f29efa2d20889990be3dbdb80650c7a82f75d728
Ketchup9743 <liamlessard20@gmail.com>
2026-09-16 13:18:54 +0000

    added a little secret :)

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
 let counter = 1;
+let clickCount = 0;
+
+const container = document.getElementById("popup-container");
+
+if (container) {
+    container.addEventListener("click", (event) => {
+        if (event.target.closest(".doom-popup")) {
+            clickCount++;
+            alert(clickCount === 25 ? "Why do you keep clicking this?" : "Nothing Here :)");
+        }
+    });
+}
 
 function spawn_doom_popup() {
-    const container = document.getElementById("popup-container");
     if (container) {
-        container.innerHTML = '<div class="doom-popup">' + counter + '</div>';
+        container.innerHTML = '<button class="doom-popup" type="button">' + counter + '</button>';
         counter++;
     }
     requestAnimationFrame(spawn_doom_popup);
