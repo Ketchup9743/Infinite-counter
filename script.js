dd84c166db90d8bfce0c63af2434fa0b77be45ef
Ketchup9743 <liamlessard20@gmail.com>
2026-09-16 13:24:51 +0000

    added another secret

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let counter = 1;
 let clickCount = 0;
 
 const container = document.getElementById("popup-container");
+const clickCounterDisplay = document.getElementById("click-counter");
 
 function show_click_message() {
     clickCount++;
+    if (clickCounterDisplay) {
+        clickCounterDisplay.textContent = clickCount;
+    }
     window.alert(clickCount % 25 === 0 ? "Why do you keep clicking this?" : "Nothing Here :)");
 }
 
