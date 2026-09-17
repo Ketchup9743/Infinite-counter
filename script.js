b5df3a7be5ccbd2263180f6dec1ddce58e8b3447
Ketchup9743 <liamlessard20@gmail.com>
2026-09-17 19:57:37 +0000

    fixed the counter

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function show_click_message() {
         clickCounterDisplay.textContent = clickCount;
     }
     window.alert(clickCount % 25 === 0 ? "Why do you keep clicking this?" : "Nothing Here :)");
+}
+
 function spawn_doom_popup() {
     if (container) {
         let popup = container.querySelector(".doom-popup");
@@ -23,4 +25,5 @@ function spawn_doom_popup() {
     }
     requestAnimationFrame(spawn_doom_popup);
 }
-requestAnimationFrame(spawn_doom_popup);}
+
+requestAnimationFrame(spawn_doom_popup);
